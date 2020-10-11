458d26d7affbe3679d3302b8e3014b7b9edf0929
SuhitAgarwal <suhitagarwal@gmail.com>
2020-10-11 17:54:32 +0530

    deploy!

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaCode, FaPenNib } from "react-icons/fa";
+
+function LandingPage() {
+    return (
+        <>
+        <div className="app">
+            <FaPenNib style={{ fontSize: '4rem' }} /><br />
+            <span style={{ fontSize: '2rem' }}>Let's Start Writing!</span>
+        </div>
+        </>
+    )
+}
+
+export default LandingPage
